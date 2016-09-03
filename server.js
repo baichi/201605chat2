b308448fc460ff4c046e08be68b6cd33d8577543
zhufengnodejs <zhufengnodejs@126.com>
2016-09-03 15:46:05 +0800

    绘制首页

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(path.resolve('public')));
 //当客户端访问/的时候，执行对应回调函数，返回index.html文件
 app.get('/',function(req,res){
-    res.sendFile('./index.html',path.resolve('src'));
+    res.sendFile('./index.html',{root:path.resolve('src')});
 });
 
 app.listen(80);
